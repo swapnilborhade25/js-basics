c5234226b6ef580c554400d9d0f6cc4ca3867e77
swapnilborhade25 <swapnilborhade25@gmail.com>
2026-08-29 17:13:03 +0530

    add first js file

---
+++ first.js
@@ -0,0 +1,19 @@
+console.log("Hello Swap");
+
+let name = "Sanchit";
+const age = 23; // can't update value once assigned
+let x  = null;
+let y = undefined;
+let isFollow = false; // boolean
+
+console.log(age);
+
+const student = {
+     name : "Rahul",
+    age : 27,
+    college : "JSPM"
+}
+console.log(student);
+console.log (student.age);
+// or
+console.log(student["name"]);
